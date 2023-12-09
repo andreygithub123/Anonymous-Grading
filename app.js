6bc399b2cca838db8530908301f5ca208ca85f9a
Andrew Diaconu <diaconu1andrei21@stud.ase.ro>
2023-12-09 11:40:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('pinguinul futacios');
+
+function dividingNumbers(a,b)
+{
+    if(b!=0)
+    {
+        return a/b;
+    }else if ( a!=0 )
+    {
+        return b/a
+    }else{
+        throw new Error("Impossible. Try again with different numberes!");
+    }
+        
+}
