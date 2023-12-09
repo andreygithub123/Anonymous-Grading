1ab44a6c7f59f0f3a03775fa86f6a30f0196cae9
Andrew Diaconu <diaconu1andrei21@stud.ase.ro>
2023-12-09 13:52:21 +0200

    Update3
    -created Livrable class in Classes

---
+++ Classes/livrable.js
@@ -0,0 +1,36 @@
+class Livrable{
+
+    constructor(phaseNo,deliveryDate,comments,video,linkGit)
+    {
+        this.phaseNo=phaseNo;
+        this.deliveryDate=deliveryDate;
+        this.comments=comments;
+        this.video=video;
+        this.linkGit=linkGit;
+    }
+
+    // get phaseNo(){
+    //     return this.phaseNo;
+    // }
+
+    // set phaseNo(newPhase){
+    //     this.phaseNo=newPhase;
+    // }
+
+    // functionx(){
+
+    // }
+    
+}
+
+//export default Livrable;
+
+//check
+//Date(year,month,day) - constructor for 00:00:00 creation
+// const l1=new Livrable(1,new Date("2023-12-09"),"blablas fas aslf afla flafl ","acaca","hhtps://github/theoretically-andrei.com");
+// console.log(l1)
+
+// if(l1.deliveryDate=="Sat Dec 09 2023 02:00:00 GMT+0200 (Eastern European Standard Time)")
+// {
+//     console.log("Adevarata!");
+// }
