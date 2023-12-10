e306419a1ae6eae22f220436579a77788500fba6
Alexandru Mihai Ionel <126344850+ionelalexandru01@users.noreply.github.com>
2023-12-10 14:37:07 +0200

    Update 4
    -Created Routers

---
+++ Routers/about.js
@@ -0,0 +1,7 @@
+import express from "express"
+
+const aboutRouter = express.Router();
+
+
+
+export default aboutRouter
--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // import classes
 import express from 'express'
 import Student from './Classes/student.js'
+import homeRouter from './Routers/home.js'
+import aboutRouter from './Routers/about.js';
 
+
+// create an Express Server
 const app = express();
+// use express.json for parsing into/from json format
 app.use(express.json());
-// app.use('/',router)
+
+// Routers that will be used in the server
+app.use('/',homeRouter)
+app.use('/',aboutRouter)
 
 
 
+
+
+
+// Server port
 app.listen(8080);
