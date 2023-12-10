db95df2ebcfb857b9047fed04ea58a4bee80ea97
Alexandru Mihai Ionel <126344850+ionelalexandru01@users.noreply.github.com>
2023-12-11 00:48:59 +0200

    -DeleteALL

--- Routers/about.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-
-const aboutRouter = express.Router();
-
-
-
-export default aboutRouter
--- Routers/home.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-
-const homeRouter = express.Router();
-
-
-
-export default homeRouter
