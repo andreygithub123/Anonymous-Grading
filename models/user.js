3cb1109650da5e7fb92985ad007ee0f24deb18fe
Alexandru Mihai Ionel <126344850+ionelalexandru01@users.noreply.github.com>
2023-12-11 19:23:56 +0200

    Update1

--- models/user.js
+++ models/user.js
@@ -5,15 +5,29 @@ const User = sequelize.define("User", {
     id:{
         type: DataTypes.INTEGER,
         primaryKey:true,
+        allowNull:false,
         autoIncrement:true,
     },
-    fullName : DataTypes.STRING,
+    fullName :{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
     email:{
         type:DataTypes.STRING,
         validate: {
             isEmail:true,
         },
     },
+    type:{
+        type: DataTypes.ENUM,
+        values: ['PM', 'PROF']
+    },
+    teamId:
+    {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    }
+
 });
 
 module.exports=User;
